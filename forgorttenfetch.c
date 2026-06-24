9aaa786c5a74782dd3e95d1f868900bd52df6bf7
justforgot <minecraftboss7829@gmail.com>
2026-06-24 01:13:30 -0700

    Add forgorttenfetch.c

---
+++ forgorttenfetch.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("OS:im so sorry i have forgotten\nHost: sorry i forgot\nKernel:uhhh, unsure\nUptime: how am i supposed to remember if you dont?\nPackages: can you not remember ts for me?\nShell: told you i forgot i think idk i forgot\nDisplay: you expect me to remember ts?\nWM: idk i forgor\nTerminal: forgot\nTerminal Font: dunno\nCPU: idk i forgor\nGPU: idfk\nMemory: idk, forgot\nSwap:\nDisk (/): i has forgortten\nLocal IP (eth0): forgort\nLocale: i have forgortten\n");
+  return 0;
+}
